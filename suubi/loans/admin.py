5cc777a0f315e8862679029f2acd7b4d93ec61b2
kimura-khamali <kimurakhamali@gmail.com>
2024-07-29 11:29:24 +0300

    added members

--- suubi/loans/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Loans
-admin.site.register(Loans)
-
-
-
-# Register your models here.
--- suubi/milk_sales/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import MilkSales
-
-admin.site.register(MilkSales)
