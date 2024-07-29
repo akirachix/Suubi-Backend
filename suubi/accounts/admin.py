1951ac7b59438099c21f14da188f351ab6c8ed1e
kimura-khamali <kimurakhamali@gmail.com>
2024-07-29 11:30:54 +0300

    added members

--- suubi/accounts/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Accounts
-admin.site.register(Accounts)
-
-# Register your models here.
--- suubi/suubi/settings.py
+++ suubi/suubi/settings.py
@@ -37,10 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'members',
-    'accounts',
-    'loans',
-    'milk_sales'
+    'members'
+      
     
 ]
 
